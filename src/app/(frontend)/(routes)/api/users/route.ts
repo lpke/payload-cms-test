02018df187578c2189562fe886a1ac1aca400e57
Luke <luke@lpdev.io>
2024-12-09 16:43:29 +1100

    chore: update to reflect new db tables

--- src/app/(frontend)/(routes)/api/users/route.ts
+++ src/app/(frontend)/(routes)/api/users/route.ts
@@ -1,7 +1,7 @@
 import { dbFetch } from '@/api/db-fetch';
 
 export async function GET(request: Request) {
-  const data = await dbFetch(`SELECT * FROM public.games`);
+  const data = await dbFetch(`SELECT * FROM public.users`);
 
   return Response.json(
     { data: data },
